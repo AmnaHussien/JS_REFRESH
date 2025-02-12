7c5deecb02571baa38b429aff51bc6e34025093a
AmnaHussien <amnahussin88@gmail.com>
2025-02-12 11:15:46 +0200

    s

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log("Hello World");
+/* how to declare variables in js
+1- var (variable will be used for all code, can change letter)
+2- let (the variable will have scoped declartion)
+3- const (the value of var canot be change)
+*/
+var AppleCase;
+var BananaCase;
+var a = 4, b = 16;
+a += 5;
+b /= 3;
+// how to apear double quotation mark
+var Myname = " my Firstname is \"Amna \" and My Lastname is \"Ahmed \""
+namelength = Myname.length;
+secondletter = Myname[2];
+console.log(secondletter);
+console.log(namelength);
+console.log(Myname);
+AppleCase = "Apple\"\"";
+BananaCase = "Banana";
+console.log(AppleCase+BananaCase);
+myvar=4;
+myvar--;
+let Nama = "Anni\t"
+var fullname= "my name is " + Nama + "Anderew"
+console.log(fullname)
+var rem = 11 % 3;
+console.log(rem);
+console.log(myvar);
+console.log(AppleCase);
